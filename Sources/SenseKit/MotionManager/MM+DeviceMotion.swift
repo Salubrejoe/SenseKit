cab89bb2f9d24975bba47da4d86984582bb7aad0
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2024-10-29 16:57:11 +0000

    Fixed it i thik

--- Sources/SenseKit/MotionManager/MM+DeviceMotion.swift
+++ Sources/SenseKit/MotionManager/MM+DeviceMotion.swift
@@ -28,10 +28,9 @@ public extension MotionManager {
     self.userAcceleration = DeviceMotionCalculator.shared.calculateUserAcceleration(from: motionData)
     self.heading          = DeviceMotionCalculator.shared.calculateHeading(from: motionData)
     let field = motionData.magneticField.field
-    let x = Measurement<UnitMagneticField>(value: field.x.magnitude, unit: .microteslas)
-    let y = Measurement<UnitMagneticField>(value: field.y.magnitude, unit: .microteslas)
-    let z = Measurement<UnitMagneticField>(value: field.z.magnitude, unit: .microteslas)
-    print("x: \(x.value), y: \(y.value), z: \(z.value)")
+    let x = Measurement<UnitMagneticField>(value: field.x, unit: .microteslas)
+    let y = Measurement<UnitMagneticField>(value: field.y, unit: .microteslas)
+    let z = Measurement<UnitMagneticField>(value: field.z, unit: .microteslas)
     let magnetometer = Vector(x: x, y: y, z: z)
     self.magnetometer = magnetometer
   }
