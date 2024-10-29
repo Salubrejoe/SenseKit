71d6631d005e1ab56dea34582acbbf2403ef8a0c
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2024-10-29 16:29:32 +0000

    Porcaddio guarda

--- Sources/SenseKit/ViewModifiers/MagnetEffect/MagnetEffect.swift
+++ Sources/SenseKit/ViewModifiers/MagnetEffect/MagnetEffect.swift
@@ -3,8 +3,8 @@ import SwiftUI
 
 
 public extension View {
-  func magnetEffect(maxOffset: CGFloat = 20, scaleMultiplier: CGFloat = 0.1, animation: Animation = .linear) -> some View {
-    modifier(MagnetEffect(maxOffset: maxOffset, scaleMultiplier: scaleMultiplier, animation: animation))
+  func magnetEffect(maxOffset: CGFloat = 20) -> some View {
+    modifier(MagnetEffect(maxOffset: maxOffset))
   }
 }
 
@@ -14,21 +14,18 @@ public struct MagnetEffect: ViewModifier {
   @Environment(MotionManager.self) var stream: MotionManager?
   
   public let maxOffset: CGFloat
-  public let scaleMultiplier: CGFloat
-  public let animation: Animation
   
   public func body(content: Content) -> some View {
     
     if let stream {
       
       content
-        .scaleEffect(stream.magnAxisZScale(multiplier: scaleMultiplier))
         .offset(
           x: stream.magnAxisXOffset(max: maxOffset),
           y: stream.magnAxisYOffset(max: maxOffset)
         )
         .animation(
-          animation,
+          .linear,
           value: stream.magnetometer
         )
       
