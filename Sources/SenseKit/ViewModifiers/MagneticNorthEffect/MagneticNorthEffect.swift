f69564b7f7db54d349ba23a173d6fc44f2ba2b89
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2024-10-30 17:45:25 +0000

    Made heading modifier instead

--- Sources/SenseKit/ViewModifiers/MagneticNorthEffect/MagneticNorthEffect.swift
+++ Sources/SenseKit/ViewModifiers/MagneticNorthEffect/MagneticNorthEffect.swift
@@ -18,50 +18,21 @@ public struct MagneticNorthEffect: ViewModifier {
     
     if let stream {
       content
-        .rotation3DEffect(
-          angleToNorth(),
-          axis: (x: normalizedMagneticVector.y, y: -normalizedMagneticVector.x, z: 0)
-        )
+        .rotationEffect(headingAngle, anchor: .bottom)
         .animation(
           animation,
-          value: magnetometer
+          value: headingAngle
         )
     } else {
       content
     }
   }
   
-  private var magnetometer: Vector<UnitMagneticField> {
-    stream?.magnetometer ?? Vector()
+  private var headingAngle: Angle {
+    Angle(degrees: rawHeading)
   }
   
-  private var normalizedMagneticVector: (x: CGFloat, y: CGFloat) {
-    // Calculate the magnitude of the magnetic field vector
-    let magnitude = sqrt(pow(magnetometer.x.value, 2) + pow(magnetometer.y.value, 2))
-    
-    // Avoid division by zero; if magnitude is too low, return zero offsets
-    guard magnitude > 0 else {
-      return (x: 0, y: 0)
-    }
-    
-    // Normalize the magnetic field values for the X and Y components
-    return (
-      x: magnetometer.x.value / magnitude,
-      y: magnetometer.y.value / magnitude
-    )
-  }
-  
-  public func axisXOffset(max maxOffset: CGFloat) -> CGFloat {
-    maxOffset * normalizedMagneticVector.x
-  }
-  
-  public func axisYOffset(max maxOffset: CGFloat) -> CGFloat {
-    -1 * maxOffset * normalizedMagneticVector.y
-  }
-  
-  public func angleToNorth(_ multiplier: CGFloat = 1) -> Angle {
-    // Calculate the angle in radians and apply the multiplier
-    let angle = atan2(normalizedMagneticVector.y, normalizedMagneticVector.x)
-    return Angle(radians: Double(angle) * Double(multiplier))
+  private var rawHeading: Double {
+    stream?.heading.value ?? 0.0
   }
 }
