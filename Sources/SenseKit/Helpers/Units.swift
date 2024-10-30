cee44aaa012c2878db6e5cd82e0fc3050b6da8e0
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2024-10-30 10:04:55 +0000

    baseUnit() now public

--- Sources/SenseKit/Helpers/Units.swift
+++ Sources/SenseKit/Helpers/Units.swift
@@ -5,16 +5,18 @@ import Foundation
 public class UnitAngularVelocity: Dimension {
   static let radiansPerSecond = UnitAngularVelocity(symbol: "rad/s", converter: UnitConverterLinear(coefficient: 1.0))
   
-  func baseUnit() -> UnitAngularVelocity {
+  public func baseUnit() -> UnitAngularVelocity {
     return .radiansPerSecond
   }
+  
+  public static let degreesPerSecond = UnitAngularVelocity(symbol: "°/s", converter: UnitConverterLinear(coefficient: .pi / 180))
 }
 
 public class UnitMagneticField: Dimension {
   static let microteslas = UnitMagneticField(symbol: "µT", converter: UnitConverterLinear(coefficient: 1.0))
   static let gauss = UnitMagneticField(symbol: "G", converter: UnitConverterLinear(coefficient: 100.0)) // 1 Gauss = 100 µT
   
-  func baseUnit() -> UnitMagneticField {
+  public func baseUnit() -> UnitMagneticField {
     return .microteslas
   }
 }
