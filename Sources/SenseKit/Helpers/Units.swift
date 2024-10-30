41457d10ec3f35a11da39dd26fac0ef3b273f4bd
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2024-10-30 10:09:51 +0000

    StackOverflowed my soplution

--- Sources/SenseKit/Helpers/Units.swift
+++ Sources/SenseKit/Helpers/Units.swift
@@ -5,8 +5,8 @@ import Foundation
 public class UnitAngularVelocity: Dimension {
   static let radiansPerSecond = UnitAngularVelocity(symbol: "rad/s", converter: UnitConverterLinear(coefficient: 1.0))
   
-  public func baseUnit() -> UnitAngularVelocity {
-    return .radiansPerSecond
+  override class public func baseUnit() -> Self {
+    Self(symbol: "rad/s")
   }
   
   public static let degreesPerSecond = UnitAngularVelocity(symbol: "°/s", converter: UnitConverterLinear(coefficient: .pi / 180))
@@ -16,7 +16,7 @@ public class UnitMagneticField: Dimension {
   static let microteslas = UnitMagneticField(symbol: "µT", converter: UnitConverterLinear(coefficient: 1.0))
   static let gauss = UnitMagneticField(symbol: "G", converter: UnitConverterLinear(coefficient: 100.0)) // 1 Gauss = 100 µT
   
-  public func baseUnit() -> UnitMagneticField {
-    return .microteslas
+  override class public func baseUnit() -> Self {
+    Self(symbol: "µT")
   }
 }
