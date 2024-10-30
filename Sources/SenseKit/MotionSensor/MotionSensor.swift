4f7ac7d6bc4c5fd6104c02ff3e4be00a217c66e9
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2024-10-30 10:00:12 +0000

    Che casino

--- Sources/SenseKit/MotionSensor/MotionSensor.swift
+++ Sources/SenseKit/MotionSensor/MotionSensor.swift
@@ -6,7 +6,15 @@ import CoreMotion
 public class MotionSensor {
   public static let stream = MotionSensor()
   
-  public let motionManager = CMMotionManager()
+  public let motion = CMMotionManager()
+  
+  public var attitude         : Vector<UnitAngle> = .zero
+  public var gravity          : Vector<UnitAcceleration> = .zero
+  public var userAcceleration : Vector<UnitAcceleration> = .zero
+  public var rotationRate     : Vector<UnitAngularVelocity> = .zero
+  public var magnetometer     : Vector<UnitMagneticField> = .zero
+  public var heading          : Measurement<UnitAngle> = .init(value: 0, unit: .degrees)
+  
   
   init(updateInterval: TimeInterval = 0.1) {
     self.updateInterval = updateInterval
@@ -15,8 +23,19 @@ public class MotionSensor {
   
   deinit { stop() }
   
+  public var updateInterval: TimeInterval {
+    get {
+      motion.deviceMotionUpdateInterval
+    }
+    set {
+      motion.deviceMotionUpdateInterval  = newValue
+      motion.accelerometerUpdateInterval = newValue
+      motion.magnetometerUpdateInterval  = newValue
+    }
+  }
+  
   public func setOnUpdateHandler(_ handler: @escaping () -> Void) {
-    motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: { deviceMotion, error in
+    motion.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: { deviceMotion, error in
       handler()
     })
   }
@@ -24,28 +43,23 @@ public class MotionSensor {
 
 
 
-
 // MARK: - .init() HELPERS
 public extension MotionSensor {
+  
   func start() {
-    if motionManager.isDeviceMotionAvailable {
-      motionManager.startDeviceMotionUpdates()
-    }
-    
-    if motionManager.isAccelerometerAvailable {
-      motionManager.startAccelerometerUpdates()
-    }
-    
-    if motionManager.isMagnetometerAvailable {
-      motionManager.startMagnetometerUpdates()
+    do {
+      try startDeviceMotion()
+      try startMagnetometer()
+    } catch {
+      handleError(error)
     }
   }
   
   func stop() {
-    motionManager.stopMagnetometerUpdates()
-    motionManager.stopDeviceMotionUpdates()
-    if motionManager.isAccelerometerAvailable {
-      motionManager.stopAccelerometerUpdates()
+    motion.stopMagnetometerUpdates()
+    motion.stopDeviceMotionUpdates()
+    if motion.isAccelerometerAvailable {
+      motion.stopAccelerometerUpdates()
     }
   }
   
@@ -55,8 +69,66 @@ public extension MotionSensor {
 }
 
 
+// MARK: - DeviceMotion
+public extension MotionSensor {
+  
+  func startDeviceMotion() throws {
+    guard motion.isDeviceMotionAvailable else {
+      throw MotionSensorError.deviceMotionUnavailable
+    }
+    
+    self.motion.startDeviceMotionUpdates(to: .current!) { [weak self] motionData, error in
+      guard let motionData, error == nil else {
+        if let error {
+          self?.handleError(error)
+          return
+        }
+        return
+      }
+      
+      DispatchQueue.main.async { self?.process(motionData) }
+    }
+  }
+  
+  private func process(_ motionData: CMDeviceMotion) {
+    self.attitude         = DeviceMotionCalculator.shared.calculateAttitude(from: motionData)
+    self.rotationRate     = DeviceMotionCalculator.shared.calculateRotationRate(from: motionData)
+    self.gravity          = DeviceMotionCalculator.shared.calculateGravity(from: motionData)
+    self.userAcceleration = DeviceMotionCalculator.shared.calculateUserAcceleration(from: motionData)
+    self.heading          = DeviceMotionCalculator.shared.calculateHeading(from: motionData)
+//    self.magnetometer     = DeviceMotionCalculator.shared.calculateMagneticFIeld(from: motionData)
+  }
+}
+
+
+// MARK: - Magnetometer
+public extension MotionSensor {
+  
+  func startMagnetometer() throws {
+    guard motion.isMagnetometerAvailable else {
+      throw MotionSensorError.magnetometerUnavailable
+    }
+    
+    self.motion.magnetometerUpdateInterval = updateInterval
+    self.motion.showsDeviceMovementDisplay = true
+    
+    self.motion.startMagnetometerUpdates(to: .current!) { [weak self] data, error in
+      
+      guard let data else { return }
+      DispatchQueue.main.async {
+        self?.magnetometer = Vector(
+          x: Measurement(value: data.magneticField.x, unit: .microteslas),
+          y: Measurement(value: data.magneticField.y, unit: .microteslas),
+          z: Measurement(value: data.magneticField.z, unit: .microteslas)
+        )
+      }
+    }
+  }
+}
+
+
 // MARK: - ERROR
-public enum MotionManagerError: Error {
+public enum MotionSensorError: Error {
   case motionUnavailable
   case deviceMotionUnavailable
   case magnetometerUnavailable
