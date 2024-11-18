c102eacfd5c59d9011b88b8ae2251cc09cc557a4
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2024-11-18 09:47:52 +0000

    DIO PORCONE GUARDA

--- Package.swift
+++ Package.swift
@@ -1,37 +1,24 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SenseKit",
-    platforms: [
-      .iOS(.v17)   // Specifies iOS 17 as the minimum required version
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SenseKit",
-            targets: ["SenseKit"]),
-    ],
-    dependencies: [
-      .package(url: "https://github.com/AudioKit/AudioKit.git", from: "5.6.4"),
-      .package(url: "https://github.com/AudioKit/AudioKitEX", from: "5.6.0"),
-      .package(url: "https://github.com/AudioKit/SoundpipeAudioKit", from: "5.6.1"),
-      .package(url: "https://github.com/AudioKit/AudioKitUI", from: "0.3.7"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SenseKit",
-            dependencies: [
-              .product(name: "AudioKit", package: "AudioKit"),
-              .product(name: "AudioKitEX", package: "AudioKitEX"),
-              .product(name: "SoundpipeAudioKit", package: "SoundpipeAudioKit"),
-              .product(name: "AudioKitUI", package: "AudioKitUI"),
-            ]
-          ),
-    ],
-    swiftLanguageModes: [.v5]
+  name: "SenseKit",
+  platforms: [
+    .iOS(.v16)
+  ],
+  products: [
+    .library(
+      name: "SenseKit",
+      targets: ["SenseKit"]),
+  ],
+  targets: [
+    .target(
+      name: "SenseKit",
+      dependencies: [
+      ]
+    ),
+  ],
+  swiftLanguageModes: [.v5]
 )
--- Sources/SenseKit/Views/SKTunerView.swift
+++ Sources/SenseKit/Views/SKTunerView.swift
@@ -1,31 +1,31 @@
-
-import SwiftUI
-
-
-struct SKTunerView: View {
-  @State var 🎻 = AKConductor()
-  
-  var body: some View {
-    VStack {
-      Text("\(🎻.data.pitch, specifier: "%0.1f") hz")
-      
-      Spacer()
-      
-      Text("\(🎻.data.noteNameWithSharps)")
-        .font(.system(size: 200))
-        
-      
-      Spacer()
-      
-      NodeOutputView(🎻.tappableNodeB)
-        .clipped()
-      
-    }
-    .onAppear(perform: 🎻.start)
-    .onDisappear(perform: 🎻.stop)
-  }
-}
-
-#Preview {
-  TunerView()
-}
+//
+//import SwiftUI
+//
+//
+//struct SKTunerView: View {
+//  @State var 🎻 = AKConductor()
+//  
+//  var body: some View {
+//    VStack {
+//      Text("\(🎻.data.pitch, specifier: "%0.1f") hz")
+//      
+//      Spacer()
+//      
+//      Text("\(🎻.data.noteNameWithSharps)")
+//        .font(.system(size: 200))
+//        
+//      
+//      Spacer()
+//      
+//      NodeOutputView(🎻.tappableNodeB)
+//        .clipped()
+//      
+//    }
+//    .onAppear(perform: 🎻.start)
+//    .onDisappear(perform: 🎻.stop)
+//  }
+//}
+//
+//#Preview {
+//  TunerView()
+//}
