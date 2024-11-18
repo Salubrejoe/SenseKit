5c57606400a8d8b9b564c4749da30946705839be
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2024-11-18 09:51:02 +0000

    Reverted

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,19 +6,13 @@ import PackageDescription
 let package = Package(
   name: "SenseKit",
   platforms: [
-    .iOS(.v16)
+    .iOS(.v17)
   ],
   products: [
-    .library(
-      name: "SenseKit",
-      targets: ["SenseKit"]),
+    .library(name: "SenseKit", targets: ["SenseKit"]),
   ],
   targets: [
-    .target(
-      name: "SenseKit",
-      dependencies: [
-      ]
-    ),
+    .target(name: "SenseKit", dependencies: []),
   ],
-  swiftLanguageModes: [.v5]
+  swiftLanguageModes: [.v6]
 )
